5725ad2987a217e669347e72d269ceb03aa1d545
Roman Kananovich <roman.kananovich@gmail.com>
2019-03-31 22:29:15 +0300

    added check is Neo or Clone in MatrixHackerTests.m

--- Tasks/Task2/RSSchool_T2/RSSchool_T2Tests/MatrixHackerTests.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2Tests/MatrixHackerTests.m
@@ -7,9 +7,39 @@
 @end
 
 @interface Char : NSObject <Character>
+@property (nonatomic, retain) NSString* name;
+@property (nonatomic, assign) BOOL isClone;
+
+- (NSString *)name;
+- (BOOL)isClone;
++ (instancetype)createWithName:(NSString *)name isClone:(BOOL)clone;
 @end
 
 @implementation Char
+- (NSString *)name
+{
+    return [self name];
+}
+- (BOOL)isClone
+{
+    return [self isClone];
+}
++ (instancetype)createWithName:(NSString *)name isClone:(BOOL)clone
+{
+    Char * newChar = [[[Char alloc] init] autorelease];
+    
+    newChar.name = name;
+    if ([name isEqualToString:@"Neo"])
+    {
+        newChar.isClone = NO;
+    }
+    else
+    {
+        newChar.isClone = YES;
+    }
+    
+    return newChar;
+}
 
 @end
 
@@ -23,8 +53,9 @@
 - (void)test1 {
   __block NSInteger counter = 0;
   [self.hacker injectCode:^id<Character>(NSString *name) {
+      
     counter += 1;
-    return [Char new];
+    return [Char createWithName:name isClone:YES];
   }];
   [self.hacker runCodeWithData:self.people];
   XCTAssertTrue(self.people.count == counter);
